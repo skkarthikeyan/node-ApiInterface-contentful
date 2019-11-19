c86f32b8c91836cbecb7ed29cda398649965306a
Karthik <karthik@aleph-labs.com>
2019-11-19 11:04:32 +0800

    added process env port

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const routes = require('./routes/route');
 require("babel-polyfill");
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log("Server is listening on port: 3000");
+app.listen(PORT, () => {
+  console.log("Server is listening on port: "+PORT);
 });
 
 app.use("/api", routes);
